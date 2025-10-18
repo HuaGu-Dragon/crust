5ba9aef1130344136bddfa2d0978709a5e9ec4ae
俺不叫化咕龙 <1801943622@qq.com>
2025-10-18 20:46:19 +0800

    Add state checks and IPv4 header reuse
    Validate ACK/sequence numbers and receive window and add a
    between_wrapping helper. Store and reuse Ipv4Header in Connection for
    checksum calculation and packet writes. Add State::is_synchronized and
    basic state-handling skeletons.

--- src/tcp.rs
+++ src/tcp.rs
@@ -2,15 +2,24 @@ use etherparse::{IpNumber, Ipv4Header, Ipv4HeaderSlice, TcpHeader, TcpHeaderSlic
 use tun::Device;
 
 enum State {
-    Listen,
     SynRcv,
     Established,
+}
+
+impl State {
+    fn is_synchronized(&self) -> bool {
+        match self {
+            State::SynRcv => false,
+            State::Established => true,
+        }
+    }
 }
 
 pub struct Connection {
     state: State,
     send: SendSequenceSpace,
     recv: RecvSequenceSpace,
+    iph: Ipv4Header,
 }
 
 struct SendSequenceSpace {
@@ -49,7 +58,7 @@ impl Connection {
         payload: &[u8],
     ) -> Result<Option<Self>, std::io::Error> {
         println!(
-            "\tTCP packet: {}:{} -> {}:{}, seq: {}, ack: {}, window: {}, payload length: {}",
+            "RST packet: {}:{} -> {}:{}, seq: {}, ack: {}, window: {}, payload length: {}",
             iph.source_addr(),
             tcp_header.source_port(),
             iph.destination_addr(),
@@ -65,7 +74,7 @@ impl Connection {
         }
 
         let iss = 0;
-        let c = Connection {
+        let mut c = Connection {
             state: State::SynRcv,
             send: SendSequenceSpace {
                 iss,
@@ -82,6 +91,7 @@ impl Connection {
                 wnd: tcp_header.window_size(),
                 up: false,
             },
+            iph: Ipv4Header::new(0, 64, IpNumber::TCP, iph.destination(), iph.source()).unwrap(),
         };
 
         let mut syn_ack = TcpHeader::new(
@@ -93,25 +103,19 @@ impl Connection {
         syn_ack.acknowledgment_number = c.recv.nxt;
         syn_ack.syn = true;
         syn_ack.ack = true;
-
-        let ip = Ipv4Header::new(
-            syn_ack.header_len_u16(),
-            64,
-            IpNumber::TCP,
-            iph.destination(),
-            iph.source(),
-        )
-        .unwrap();
+        c.iph
+            .set_payload_len(syn_ack.header_len())
+            .expect("Failed to set payload len");
 
         syn_ack.checksum = syn_ack
-            .calc_checksum_ipv4(&ip, &[])
+            .calc_checksum_ipv4(&c.iph, &[])
             .expect("failed to compute checksum");
 
         let mut buf = [0u8; 1500];
 
         let unwritten = {
             let mut unwritten = &mut buf[..];
-            ip.write(&mut unwritten).unwrap();
+            c.iph.write(&mut unwritten).unwrap();
             syn_ack.write(&mut unwritten).unwrap();
             unwritten.len()
         };
@@ -124,7 +128,65 @@ impl Connection {
         iph: Ipv4HeaderSlice,
         tcp_header: TcpHeaderSlice,
         payload: &[u8],
-    ) -> Result<usize, std::io::Error> {
-        Ok(0)
+    ) -> Result<(), std::io::Error> {
+        if !between_wrapping(
+            self.send.una,
+            tcp_header.acknowledgment_number(),
+            self.send.nxt.wrapping_add(1),
+        ) {
+            if !self.state.is_synchronized() {
+                // TODO: send a RST packet
+            }
+            return Ok(());
+        };
+        let start = self.recv.nxt.wrapping_sub(1);
+        let seqn = tcp_header.sequence_number();
+        let end = self.recv.nxt.wrapping_add(self.recv.wnd as u32);
+        let mut n = payload.len() as u32;
+        if tcp_header.fin() {
+            n += 1;
+        }
+        if tcp_header.syn() {
+            n += 1;
+        }
+        if n == 0 {
+            if self.recv.wnd == 0 {
+                if seqn != self.recv.nxt {
+                    return Ok(());
+                }
+            } else if !between_wrapping(start, seqn, end) {
+                return Ok(());
+            }
+        } else {
+            if self.recv.wnd == 0 {
+                return Ok(());
+            } else if !between_wrapping(start, seqn, end)
+                && !between_wrapping(start, seqn.wrapping_add(n - 1), end)
+            {
+                return Ok(());
+            }
+        }
+
+        match self.state {
+            State::Listen => todo!(),
+            State::SynRcv => {
+                if !tcp_header.ack() {
+                    return Ok(());
+                }
+                todo!()
+            }
+            State::Established => todo!(),
+        }
+    }
+}
+
+fn between_wrapping<T>(start: T, x: T, end: T) -> bool
+where
+    T: PartialOrd + Ord,
+{
+    match start.cmp(&end) {
+        std::cmp::Ordering::Less => !(end >= start && end <= x),
+        std::cmp::Ordering::Equal => false,
+        std::cmp::Ordering::Greater => end < start && end > x,
     }
 }
